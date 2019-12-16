8203a7fd6a48f18b6f98350ad1722c5c923619c6
HamzaQureshi1 <hqureshi6594@gmail.com>
2019-12-16 12:26:42 +0000

    final

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 
 class SortBy extends Component {
+  state = {
+    sort_by: null
+  };
   render() {
+    // console.log(this.state.sort_by);
     return (
       <div>
-        <select>
-          <option value="created_at" onClick={this.handleChange}>
-            Created at
-          </option>
-          <option value="votes" onClick={this.handleChange}>
-            Votes
-          </option>
-          <option value="comment_count" onClick={this.handleChange}>
-            Comment count
-          </option>
+        <select
+          value={this.state.sort_by}
+          name="sort_by"
+          onChange={this.handleChange}
+        >
+          <option value="created_at">Created at</option>
+          <option value="votes">Votes</option>
+          <option value="comment_count">Comment count</option>
         </select>
       </div>
     );
   }
+
+  componentDidM;
   handleChange = event => {
-    console.log(event.target.value);
+    const { name, value } = event.target;
+    console.log(value)
     const fetchArticles = this.props.function;
-    const sort_by = event.target.value;
-    fetchArticles(sort_by);
+
+    this.setState({ [name]: value });
+    fetchArticles(value);
   };
+
+  // handleSubmit = sort_by => {
+  //   const fetchArticles = this.props.function;
+  //   fetchArticles(this.state.sort_by);
+  // };
 }
 export default SortBy;
